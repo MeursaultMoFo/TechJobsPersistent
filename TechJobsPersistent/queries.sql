ed6536b5e36c6acbb9832d84dff66875025ad840
Taylor MJ <taylorjohnsoninc@gmail.com>
2021-01-04 20:51:22 -0600

    finished sql queries

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,7 +1,14 @@
 --Part 1
 -- Id int; Name varchar; EmployerId int
+-- Columns:
+-- Id int AI PK 
+-- Name longtext 
+-- EmployerId int
 
 --Part 2
+-- select * from techjobspersistent.employers where Location = "St. Louis City";
 
 --Part 3
+-- SELECT * FROM techjobspersistent.skills INNER JOIN techjobspersistent.jobskills ON techjobspersistent.skills.Id = techjobspersistent.jobskills.SkillId ORDER BY Name ASC;
+
 
